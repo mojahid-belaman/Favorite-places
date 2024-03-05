1f204da9154e212bf1f09d9888471d96526f225f
mojahid-belaman <mojahidbelaman@gmail.com>
2024-03-05 11:07:17 +0100

    create custom widget handlle list of places

---
+++ lib/widgets/place_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:favorite_places/modals/place.dart';
+import 'package:favorite_places/screens/details_place.dart';
+
+class PlaceList extends StatelessWidget {
+  const PlaceList({super.key, required this.places});
+
+  final List<Place> places;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (ctx, index) => ListTile(
+        title: Text(
+          places[index].title,
+          style: ThemeData()
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: ThemeData().colorScheme.onPrimary, fontSize: 18),
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return DetailsPlace(
+                  place: places[index],
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
