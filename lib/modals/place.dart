3574e7853fe4f6611a22d49c86708e3b4e38c07d
mojahid-belaman <mojahidbelaman@gmail.com>
2024-03-06 13:42:17 +0100

    create new modal of location

--- lib/modals/place.dart
+++ lib/modals/place.dart
@@ -4,10 +4,21 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+class PlaceLocation {
+  const PlaceLocation(
+      {required this.lat, required this.lon, required this.address});
+
+  final double lat;
+  final double lon;
+  final String address;
+}
+
 class Place {
-  Place({required this.title, this.image}) : id = uuid.v4();
+  Place({required this.title, required this.image, this.location})
+      : id = uuid.v4();
 
   final String id;
   final String title;
   final File? image;
+  final PlaceLocation? location;
 }
