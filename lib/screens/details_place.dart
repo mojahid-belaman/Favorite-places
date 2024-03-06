6cc91a5b200465a988c80d8889476dcf5e2735a4
mojahid-belaman <mojahidbelaman@gmail.com>
2024-03-06 11:53:53 +0100

    add widget stack

--- lib/screens/details_place.dart
+++ lib/screens/details_place.dart
@@ -9,12 +9,18 @@ class DetailsPlace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(place.title),
-      ),
-      body: Center(
-        child: Text(place.title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(place.title),
+        ),
+        body: Stack(
+          children: [
+            Image.file(
+              place.image!,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            )
+          ],
+        ));
   }
 }
